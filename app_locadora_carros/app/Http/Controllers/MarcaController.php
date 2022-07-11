b59b64b1c2a73f6f2a769d91efb436bff9333ea9
Fabricio Ferreira <fabroide@gmail.com>
2022-07-10 22:22:55 -0300

    [291] - selecionando registros via GET

--- app_locadora_carros/app/Http/Controllers/MarcaController.php
+++ app_locadora_carros/app/Http/Controllers/MarcaController.php
@@ -35,7 +35,8 @@ class MarcaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $insert = Marca::create($request->all());
+        return $insert;
     }
 
     /**
