35438319a5fdea80a5f4f94c85471e6f701eba49
Fabricio Ferreira <fabroide@gmail.com>
2022-07-25 17:08:41 -0300

    [63]: Blade - Template com @extends, @sections e @yield - parte II

--- app_super_gestao/resources/views/site/sobre-nos.blade.php
+++ app_super_gestao/resources/views/site/sobre-nos.blade.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <title>Super Gestão - Sobre Nós</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="{{ asset('css/estilo_basico.css') }}">
-    </head>
-
-    <body>
-        <div class="topo">
+@extends('site.layouts.basico')
+@section('titulo', 'Sobre Nós')
+@section('conteudo')
+<div class="topo">
 
             <div class="logo">
                 <img src="img/logo.png">
@@ -50,6 +44,5 @@
                 <h2>Localização</h2>
                 <img src="{{ asset('img/mapa.png') }}">
             </div>
-        </div>
-    </body>
-</html>
+</div>
+ @endsection
